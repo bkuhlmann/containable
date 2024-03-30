e5e071dcb93f91a9a1c84cc4825e67edae41d5e2
Brooke Kuhlmann <brooke@alchemists.io>
2024-03-30 13:42:30 -0600

    Added primary Object API
    Necessary to provide an elegant interface when using this gem.

--- lib/containable.rb
+++ lib/containable.rb
@@ -6,4 +6,16 @@ require "containable/vault"
 
 # Main namespace.
 module Containable
+  def self.extended descendant
+    super
+    descendant.extend Vault.new
+  end
+
+  def self.[](register: Register, resolver: Resolver) = Vault.new(register:, resolver:)
+
+  def stub!(**)
+    require "containable/test"
+    extend Test
+    stub(**)
+  end
 end
---
+++ spec/containable_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Containable do
+  subject(:container) { Module.new { extend Containable } }
+
+  describe ".extended" do
+    it_behaves_like "a container"
+  end
+
+  describe ".[]" do
+    subject :container do
+      Module.new do
+        extend Containable[register: Containable::Register, resolver: Containable::Resolver]
+      end
+    end
+
+    it_behaves_like "a container"
+  end
+
+  describe ".stub!" do
+    subject :container do
+      stub_const "Test::Container", Module.new.extend(described_class)
+    end
+
+    before do
+      container.register(:one) { 1 }
+      container.register(:two, {a: 1, b: 2})
+      container.stub! two: 2
+    end
+
+    it_behaves_like "a stub"
+
+    it "answers primary stub" do
+      expect(container["two"]).to eq(2)
+    end
+  end
+end
